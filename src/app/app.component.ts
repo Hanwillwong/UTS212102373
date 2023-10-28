190bd09f07926dd8c9b2c163d7f0dbce4fdded2d
Hanwillwong <hanwillwong@gmail.com>
2023-10-28 17:38:10 +0700

    update

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'UTS212102373';
+  nama = 'Hanwill';
+  nim = '212102373';
+}
